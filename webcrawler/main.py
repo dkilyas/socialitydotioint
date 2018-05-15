3e4729112f5ec4009eefd11f60f1d734e13fdf2d
dkilyas <dkilyas@gmail.com>
2018-05-15 15:43:57 +0300

    The request test for the product url

--- webcrawler/main.py
+++ webcrawler/main.py
@@ -1,2 +1,11 @@
 import sys, requests
 from bs4 import BeautifulSoup
+
+def crawl(url):
+    sourceCode = requests.get(url)
+    print(sourceCode.status_code)
+
+
+
+productUrl = sys.argv[1]
+crawl(productUrl)
